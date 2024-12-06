91ea87950e430aefec9fd40eee182703cbb03611
Sgabix <marcofernando098@gmail.com>
2024-12-06 09:07:44 +0100

    commit 1

---
+++ esercizio1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabella Pitagorica</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            margin: 20px;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            width: 30px;
+            height: 30px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Tabella Pitagorica</h1>
+    <table>
+        <?php
+        $size = 10; // Dimensione della tabella
+        for ($i = 1; $i <= $size; $i++) {
+            echo "<tr>";
+            for ($j = 1; $j <= $size; $j++) {
+                echo "<td>" . ($i * $j) . "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
